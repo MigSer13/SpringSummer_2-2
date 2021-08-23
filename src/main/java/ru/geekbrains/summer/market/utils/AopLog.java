45a18a470fcf867faa985f61aacdc5ca77968012
MigSer13 <avto470@mail.ru>
2021-08-23 16:39:00 +0300

    homework 3

---
+++ src/main/java/ru/geekbrains/summer/market/utils/AopLog.java
@@ -0,0 +1,31 @@
+package ru.geekbrains.summer.market.utils;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import ru.geekbrains.summer.market.services.CategoryService;
+
+@Aspect
+@Component
+@Slf4j
+public class AopLog {
+
+    @Around("execution(public * ru.geekbrains.summer.market.services.*.*(..))")
+    public Object calculatingExecutionTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        if(!proceedingJoinPoint.getTarget().getClass().getName().contains("CategoryService")) {
+            long begin = System.currentTimeMillis();
+            Object out = proceedingJoinPoint.proceed();
+            long end = System.currentTimeMillis();
+            long duration = end - begin;
+            String message = proceedingJoinPoint.getTarget().getClass().getName() + " - " + duration + "ms";
+            System.out.println(message);
+            log.debug(message);
+            return out;
+        }
+        Object out = proceedingJoinPoint.proceed();
+        return out;
+    }
+}
+
--- src/main/resources/static/products/products.js
+++ src/main/resources/static/products/products.js
@@ -15,10 +15,10 @@ angular.module('app').controller('productsController', function ($scope, $http, 
             url: contextPath + '/api/v1/products',
             method: 'GET',
             params: {
-                'p': pageIndex
-//                min_price: $scope.filter.min_price
-//                max_price: $scope.filter.max_price,
-//                title: $scope.filter.titleLike
+                'p': pageIndex,
+                min_price: $scope.filter != null ? $scope.filter.min_price : null,
+                max_price: $scope.filter != null ? $scope.filter.max_price : null,
+                title:  $scope.filter != null ? $scope.filter.titleLike : null
             }
         }).then(function (response) {
             $scope.productsPage = response.data;
